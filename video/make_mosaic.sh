b89d4c56c6a5a4ee2abab3ed36d5aa84f390705a
Benjamin Chrétien <chretien@lirmm.fr>
2013-07-22 20:14:37 +0900

    Set the background color to white if there is missing videos.
    Also fix some errors.

--- video/make_mosaic.sh
+++ video/make_mosaic.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
-# This scripts intends to make a mosaic out of multiple videos. For now only
-# a 2x2 mosaic is supported.
+# This script intends to make a mosaic out of multiple videos. For now only
+# a 2x2 mosaic (2, 3 or 4 videos) is supported.
 # See: http://trac.ffmpeg.org/wiki/Create%20a%20mosaic%20out%20of%20several%20input%20videos
 
 # Global parameters
 NUM_VID=$#
 NUM_COLS=2
-let "NUM_ROWS=$NUM_VID/$NUM_COLS"
-MOSAIC_WIDTH=800
-MOSAIC_HEIGHT=600
-let "ELT_WIDTH=$MOSAIC_WIDTH/$NUM_COLS"
-let "ELT_HEIGHT=$MOSAIC_HEIGHT/$NUM_ROWS"
+
+if [ $NUM_VID -eq 1]; then
+    # One video is not enough to make a mosaic...
+    exit 2
+fi
+
+NUM_ROWS=$((($NUM_VID+$NUM_COLS-1)/$NUM_COLS))
+
+# Use the first video to fix the video size
+ELT_WIDTH=`ffprobe -v quiet -show_streams $1 \
+    | grep -i width \
+    | awk 'BEGIN{FS="="}{print $2}'`
+ELT_HEIGHT=`ffprobe -v quiet -show_streams $1 \
+    | grep -i height \
+    | awk 'BEGIN{FS="="}{print $2}'`
 let "ELT_RATIO=$ELT_WIDTH/$ELT_HEIGHT"
+
+let "MOSAIC_WIDTH=$NUM_COLS*$ELT_WIDTH"
+let "MOSAIC_HEIGHT=$NUM_ROWS*$ELT_HEIGHT"
 
 # ffmpeg parameter list
 param=""
@@ -23,8 +36,10 @@ do
 done
 
 # Prepare filter
-param=$param" -filter_complex "
-param=$param" \"nullsrc=size=${MOSAIC_WIDTH}x${MOSAIC_HEIGHT} [base];"
+param=$param" -filter_complex \""
+
+# We use white as the background color
+param=$param"color=size=${MOSAIC_WIDTH}x${MOSAIC_HEIGHT}:color=white [base];"
 
 let "max_index=$NUM_VID-1"
 for i in `seq 0 $max_index`
@@ -32,10 +47,20 @@ do
     param=$param" [$i:v] setpts=PTS-STARTPTS, scale='if(gt(a,${ELT_RATIO}),${ELT_WIDTH},-1)':'if(gt(a,${ELT_RATIO}),-1,${ELT_HEIGHT})' [v$i];"
 done
 
-param=$param" [base][v0] overlay=shortest=1                 [tmp1];"
-param=$param" [tmp1][v1] overlay=shortest=1:x=${ELT_WIDTH}  [tmp2];"
-param=$param" [tmp2][v2] overlay=shortest=1:y=${ELT_HEIGHT} [tmp3];"
-param=$param" [tmp3][v3] overlay=shortest=1:x=${ELT_WIDTH}:y=${ELT_HEIGHT}"
+param=$param" [base][v0] overlay=shortest=1"
+
+if [ $NUM_VID -ge 2 ]; then
+    param=$param"[tmp1]; [tmp1][v1] overlay=shortest=1:x=${ELT_WIDTH}"
+fi
+
+if [ $NUM_VID -ge 3 ]; then
+    param=$param"[tmp2]; [tmp2][v2] overlay=shortest=1:y=${ELT_HEIGHT}"
+fi
+
+if [ $NUM_VID -ge 4 ]; then
+    param=$param" [tmp3]; [tmp3][v3] overlay=shortest=1:x=${ELT_WIDTH}:y=${ELT_HEIGHT}"
+fi
+
 param=$param"\" -c:v libx264 output.mkv"
 
 echo $param
